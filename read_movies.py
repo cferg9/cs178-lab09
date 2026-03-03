d1982652ee58094378e377fdd7d3791497e635fd
Colton <cmfergie9@gmail.com>
2026-03-03 11:14:15 -0600

    update

--- read_movies.py
+++ read_movies.py
@@ -23,7 +23,7 @@ def print_movie(movie):
     title = movie.get("Title", "Unknown Title")
     year = movie.get("Year", "Unknown Year")
     ratings = movie.get("Ratings", "No ratings")
-    genre = genre.get ("Genre", "Unknown Genre")
+    genre = movie.get("Genre", "Unknown Genre")   # ← FIXED
 
     print(f"  Title  : {title}")
     print(f"  Year   : {year}")
